3667a396462debe1b5a6437a9d5657717a94ee4a
terumafu <119095669+terumafu@users.noreply.github.com>
2024-10-22 21:49:47 -0700

    step 1 completed, changed name and deno debug run

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import "./style.css";
 
 const APP_NAME = "Jerry's game";
 const app = document.querySelector<HTMLDivElement>("#app")!;
-
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const header = document.createElement("h1");
+header.innerHTML = "Jerry's Drawing Tool";
+
+app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
