1f31197a3bd756f13c4eb600d762e2202015c133
terumafu <119095669+terumafu@users.noreply.github.com>
2024-10-22 22:36:05 -0700

    step 2 complete, added draw and clear

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,38 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.append(canvas);
+
+const ctx : CanvasRenderingContext2D | null = canvas.getContext("2d");
+
+const mouse = {active: false, x: 0, y: 0};
+
+//add an observer for the canvas to detect mouse clicks
+canvas.addEventListener("mousemove", (event) =>{
+    //on mousemove, if mouse is active : draw a line from the last coordinates to the current coordinates
+    if (mouse.active == true){
+        ctx!.beginPath();
+        ctx!.moveTo(mouse.x, mouse.y);
+        ctx!.lineTo(event.offsetX, event.offsetY)
+        ctx!.stroke();
+        mouse.x = event.offsetX;
+        mouse.y = event.offsetY;
+    }
+});
+canvas.addEventListener("mousedown", (event) => {
+    mouse.x = event.offsetX;
+    mouse.y = event.offsetY;
+    mouse.active = true;
+
+});
+canvas.addEventListener("mouseup", () => {
+    mouse.active = false;
+});
+
+//create a button to clear the canvas
+const clearbutton = document.createElement("button");
+clearbutton.innerHTML = "clear";
+app.append(clearbutton);
+
+clearbutton.addEventListener("click", () => {
+    ctx!.clearRect(0, 0, canvas.width, canvas.height);
+})
